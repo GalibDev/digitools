4b475bb7997b04517ac0eb0dfcf218f52ccafadd
GalibDev <mirza.galib.palash>
2026-04-16 04:19:03 +0600

    fix base path for github and netlify

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,7 @@ import tailwindcss from "@tailwindcss/vite";
 
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: "/digitools/",
+  base: process.env.NODE_ENV === "production"
+    ? (process.env.NETLIFY ? "/" : "/digitools/")
+    : "/",
 });
